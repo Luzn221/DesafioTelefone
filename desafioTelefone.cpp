9fed68083b352c361ad38a293969939246d7b3b2
Luan Lima <luan_lima2011@hotmail.com>
2022-11-28 20:35:40 -0300

    Adicionado funcoes de telefone e add clientes

--- desafioTelefone.cpp
+++ desafioTelefone.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
-const int tam = 4;
+const int tam = 2;
 void NomesClientes(string nomes[]);
+void telefones();
+
 int main(int argc, char const *argv[])
 {
+srand(time(NULL));
     string *nome = new string[tam];
+    int *tel = new int[tam];
     NomesClientes(nome);
-    
+
     return 0;
 }
 void NomesClientes(string nomes[])
@@ -21,6 +27,21 @@ void NomesClientes(string nomes[])
     cout << endl;
     for (int i = 0; i < tam; i++)
     {
-        cout << nomes[i] << endl;
+        cout << "Nome: " << nomes[i] << endl;
+        cout << "Telefone: ";
+        telefones();
+        cout << endl;
     }
+}
+void telefones()
+{
+    vector<int> tl;
+    for (int i = 0; i < 2; i++)
+    {
+       tl.push_back(rand()% 2000 + 2000);
+    }
+    for(auto x : tl){
+        cout << x;
+        cout << "-";
+    }  
 }
