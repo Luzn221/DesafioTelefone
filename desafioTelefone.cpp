a872cbab40b1324290cc50195264f76e156b6877
Luan Lima <luan_lima2011@hotmail.com>
2022-11-28 17:29:34 -0300

    Adicionado funcao de inserir nomes dos clientes

--- desafioTelefone.cpp
+++ desafioTelefone.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
-
 using namespace std;
+
+const int tam = 4;
+void NomesClientes(string nomes[]);
 int main(int argc, char const *argv[])
 {
-   
+    string *nome = new string[tam];
+    NomesClientes(nome);
+    
     return 0;
+}
+void NomesClientes(string nomes[])
+{
+    cout << "Digite os Nome dos clientes:\n ";
+    for (int i = 0; i < tam; i++)
+    {
+        getline(cin, nomes[i]);
+    }
+    cout << endl;
+    for (int i = 0; i < tam; i++)
+    {
+        cout << nomes[i] << endl;
+    }
 }
